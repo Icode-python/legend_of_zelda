21c9bd46162c0ad45614657cff94e8bf554971f7
Icode-python <me.fast.learn@gmail.com>
2022-04-19 21:02:17 -0400

    trying to fix bad framerate

--- main.c
+++ main.c
@@ -80,7 +80,7 @@ void drawMap(){
 
 void display(){
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-    glutTimerFunc(100.0,timer,0);
+    //glutTimerFunc(1000.0,timer,0);
     drawMap();
     updateWeapon();
     //update_enemies();
